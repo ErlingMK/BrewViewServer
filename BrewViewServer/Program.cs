a4ff959bbbaed09e32317a3d6ff7db1d65a5b95c
ErlingMK <erling.m.kristiansen@gmail.com>
2020-09-22 20:27:41 +0200

    Added some logging

--- BrewViewServer/Program.cs
+++ BrewViewServer/Program.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.AzureAppServices;
 using Microsoft.IdentityModel.Logging;
 
 namespace BrewViewServer
@@ -18,12 +15,28 @@ namespace BrewViewServer
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(builder =>
+                {
+                    builder.AddAzureWebAppDiagnostics();
+                    builder.AddApplicationInsights();
+                })
+                .ConfigureServices(collection =>
+                {
+                    collection.Configure<AzureFileLoggerOptions>(options =>
+                        {
+                            options.FileSizeLimit = 5 * 1024;
+                            options.RetainedFileCountLimit = 5;
+                        })
+                        .Configure<AzureBlobLoggerOptions>(options => options.BlobName = "log.txt");
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder
                         .UseStartup<Startup>();
                 });
+        }
     }
 }
